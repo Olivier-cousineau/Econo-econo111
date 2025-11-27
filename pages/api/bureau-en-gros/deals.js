5657f20bb1cded598339838fcdee0a1fe96b8cf9
Olivier-cousineau <ocousineau40@gmail.com>
2025-11-26 22:07:36 -0500

    Fix import path and update retailer string quotes

--- pages/api/bureau-en-gros/deals.js
+++ pages/api/bureau-en-gros/deals.js
@@ -1,12 +1,12 @@
-// pages/api/deals/bureau-en-gros/index.js
+// pages/api/bureau-en-gros/deals.js
 
-import dealsHandler from "../../deals";
+import dealsHandler from '../deals';
 
 // On réutilise la même logique que /api/deals,
 // mais on force juste retailer = "bureau-en-gros" si non fourni.
 export default function handler(req, res) {
   if (!req.query.retailer) {
-    req.query.retailer = "bureau-en-gros";
+    req.query.retailer = 'bureau-en-gros';
   }
   return dealsHandler(req, res);
 }
